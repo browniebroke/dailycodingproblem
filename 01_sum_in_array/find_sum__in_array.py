82d535188d2c1fc93779ddd3faca54d81d1146c6
Bruno Alla <bruno.alla@founders4schools.org.uk>
2018-02-24 10:04:41 +0000

    Faster alternative

--- 01_sum_in_array/find_sum__in_array.py
+++ 01_sum_in_array/find_sum__in_array.py
@@ -1,11 +1,19 @@
+import random
+from timeit import timeit
+
+
 def main(array, sum_to_find):
     for i, a in enumerate(array):
-        for b in array[i:]:
-            if a + b == sum_to_find:
-                return True
+        b = sum_to_find - a
+        if b in array[i:]:
+            return True
     return False
 
 
 if __name__ == '__main__':
-    print(main([10, 23, 4, 6, 7, 2], 17))
-    print(main([10, 23, 4, 6, 8, 2], 17))
+    # Correctness
+    print(main([10, 2, 3, 4, 5, 11, 6, 7, 8, 9], 19)) # True
+    print(main([10, 2, 3, 4, 5, 11, 6, 7, 8, 9], 40)) # False
+    items = [random.choice(range(10_000)) for _ in range(10_000)]
+    res1 = timeit("main(items, 17)", number=10, globals=globals())
+    print(res1) # Duration
